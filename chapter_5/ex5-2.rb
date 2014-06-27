42375451a65fe89b187ca66286d27f152185791e
Ben Merkel <ben@merkelfamily.com>
2014-06-27 17:20:19 -0600

    This is the initial commit for chapter_5

---
+++ chapter_5/ex5-2.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+weight_kilos = weight / 2.2
+height_cm  = height * 2.54
+
+
+puts "Let's talk about %s." % name
+puts "He's %d inches tall (or %d centimeters)." % [height, height_cm]
+puts "He's %d pounds heavy (or %d kilos)." % [weight, weight_kilos]
+puts "Actually that's not too heavy."
+puts "He's got %s eyes and %s hair" % [eyes, hair]
+puts "His teeth are usually %s depending on the coffee." % teeth
+
+# this line is tricky, try to get it exactly right
+puts "If I add %d, %d, and %d I get %d." % [
+  age, height, weight, age + height + weight]
