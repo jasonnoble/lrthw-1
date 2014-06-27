e979a15ce864b5b62d6875772b6872c23f06d3b9
Ben Merkel <ben@merkelfamily.com>
2014-06-27 17:22:24 -0600

    This is the initial commit for chapter_7

---
+++ chapter_7/ex7.rb
@@ -0,0 +1,31 @@
+# outputs a string
+puts 'Mary had a little lamb.'
+# outputs the string with some string formatting
+puts 'Its fleece was white as %s.' % 'snow'
+# outputs the string
+puts 'And everywhere that Mary went.'
+# This outputs 10 periods in a row
+puts '.' * 10 # what did that do?
+# sets each of the variables equal to a string
+end1 = 'C'
+end2 = 'h'
+end3 = 'e'
+end4 = 'e'
+end5 = 's'
+end6 = 'e'
+end7 = 'B'
+end8 = 'u'
+end9 = 'r'
+end10 = 'g'
+end11 = 'e'
+end12 = 'r'
+
+# notice how we are using print instead of puts here. change it to puts
+# and see what happens.
+# Answer: puts end the line, whereas prints doesn't start a new line.
+
+# The next two lines outputs all of the string variables above.
+puts end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
+
+# this just is polite use of the terminal, try removing it
