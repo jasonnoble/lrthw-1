f52ea0439d6df12721ef0c8ec8a9a72f9bf83f1f
Ben Merkel <ben@merkelfamily.com>
2014-07-06 21:54:24 -0600

    Updated code to make Rubocop happy (used single quotes where possible)
    Update to head_volume and made adjustments to variables and string interpolation.
    
    Changed ex3-2 to ex3_floating and made sure the floating point numbers

--- chapter_3/Head_Volume.rb
+++ chapter_3/Head_Volume.rb
@@ -2,11 +2,11 @@
 
 pi = 3.14
 
-puts "What is the circumference of your head in inches?"
-head_circumference = gets.to_f     # 23 inch circumference
+puts 'What is the circumference of your head in inches?'
+
+head_circumference = gets.chomp.to_f
 
 head_radius = head_circumference / (2*pi)
+head_volume = (4.0/3)*pi*head_radius**3
 
-puts "The volume of your head is approximately #{4/3*pi*head_radius*head_radius*head_radius} cubed inches"
-
-exit
+puts "The volume of your head is approximately #{head_volume} cubed inches"
--- chapter_3/ex3_floating.rb
+++ chapter_3/ex3_floating.rb
@@ -6,28 +6,28 @@
 puts 'I will now count my chickens:'
 
 # This is dividing 30 by 6 and then adding 25
-puts 'Hens', 25 + 30 / 6
+puts 'Hens', 25.0 + 30.0 / 6.0
 
 # This is multiplying 25 by 3, then remainder of 75/4 and subtracting that from 100
-puts 'Roosters', 100 - 25 * 3 % 4
+puts 'Roosters', 100.0 - 25.0 * 3.0 % 4.0
 
 # Outputs a string
 puts 'now I will count the eggs:'
 
 # Gets remainder of 4/2 (which is zero) and integer of 1/4 (also zero) and the add/subtracts the rest of the numbers
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 # Outputs a string
 puts 'Is it true that 3 + 2 < 5 - 7?'
 
 # checks to see if 3+2 is less than 5-7. 5 is not less than -2 so it returns false.
-puts 3 + 2 < 5 - 7
+puts 3.0 + 2.0 < 5.0 - 7.0
 
 # Outputs a string and the sum of 3 + 2 on the next line
-puts 'What is 3 + 2?', 3 + 2
+puts 'What is 3 + 2?', 3.0 + 2.0
 
 # Outputs a string and the difference between 5 and 7 on the next line
-puts 'What is 5 - 7?', 5 - 7
+puts 'What is 5 - 7?', 5.0 - 7.0
 
 # Outputs a string
 puts "Oh, that's why it's false."
@@ -36,10 +36,10 @@ puts "Oh, that's why it's false."
 puts 'how about some more.'
 
 # Outputs a string with a true/false evaluation
-puts 'Is it greater?', 5 > -2
+puts 'Is it greater?', 5.0 > -2.0
 
 # Outputs a string with a true/false evaluation
-puts 'Is it greater or equal?', 5 >= -2
+puts 'Is it greater or equal?', 5.0 >= -2.0
 
 # Outputs a string with a true/false evaluation
-puts 'Is it less or equal?', 5 <= -2
+puts 'Is it less or equal?', 5.0 <= -2.0
